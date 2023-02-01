40add812673925e34476dece965ed1912312640d
yz3259 <yz3259@bath.ac.uk>
2023-02-01 14:57:09 +0100

    build files removed

--- build/CMakeFiles/swi_knmi.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for swi_knmi.
-# This may be replaced when dependencies are built.
--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/yz3259/Documents/Python_Jupyter_projects/SWI2023/swi-challenge-2023-netcdf-tutorial-master/projection
-# Build directory: /home/yz3259/Documents/Python_Jupyter_projects/SWI2023/swi-challenge-2023-netcdf-tutorial-master/projection/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
