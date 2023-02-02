8e5b125bbda27f27db06c28327e5c11662498ec6
yz3259 <yz3259@bath.ac.uk>
2023-02-02 09:46:38 +0100

    filename changed to new name system.

--- main_flow.py
+++ main_flow.py
@@ -5,14 +5,18 @@ import dill as pickle
 from glob import glob
 import matplotlib.pyplot as plt
 import transform
- 
+
 def read_snapshots(mydir):
     snapshots = {}
     lat, lon = None, None
     max_height = 0
     for fname in glob(os.path.join(mydir,'*.pkl')):
-        if '_new' in fname:
-            continue 
+        #print(fname)
+        # if '_new' in fname:
+        #     continue
+        if 'regrid' not in fname:
+            continue
+
         identification = os.path.basename(fname).replace('.pkl','')
         time = identification[:8]
         camera = identification[9:]
@@ -38,25 +42,25 @@ def find_mainflow(snapshots):
     u = []
     v = []
     times = sorted([key for key in snapshots])
-    
+
     for t0,t1 in zip(times[:-1],times[1:]):
         print(t0,"--",t1)
         # Get the 2D fourier transform of the height now and the height after 1 minute:
         s0 = np.fft.fft2(snapshots[t0])
         s1 = np.fft.fft2(snapshots[t1])
-    
+
         # Get the inverse 2D fourier transform of the scaled ratio
         ratio = (s0*np.conjugate(s1)) / np.abs(s0*np.conjugate(s1))
         q = np.fft.ifft2(ratio)
         print("   |imag(q)| = ",np.linalg.norm(np.imag(q)))
         print("   |real(q)| = ",np.linalg.norm(np.real(q)))
-    
+
         # it turns out that q is real: completely remove the imaginary part now
         q = np.real(q)
-    
+
         # The location of q's maximum is the drift vector (in pixels)
         w = np.where(q==np.max(q))
-    
+
         dx = w[1][0]
         dy = w[0][0]
         dx = dx if dx < q.shape[1]/2 else dx-q.shape[1]
@@ -66,9 +70,6 @@ def find_mainflow(snapshots):
     return u, v
 
 
-
-
-
 def shift_snapshots(snapshots, u, v):
     dx = [0]
     dy = [0]
@@ -76,8 +77,8 @@ def shift_snapshots(snapshots, u, v):
     for ui,vi in zip(u,v):
         dx.append(dx[-1]+ui)
         dy.append(dy[-1]+vi)
-    
-    
+
+
     shifted = {}
     for dxi, dyi, time in zip(dx,dy, times):
         dlat = dyi * (lat[1,0]-lat[0,0])
@@ -93,11 +94,11 @@ def show_shifted(lat,lon, snapshots, shifted):
     # plot the shifted fields to see how well they match:
     times = sorted([key for key in snapshots])
     for i,t0 in enumerate(times):
-        plt.figure(i); 
+        plt.figure(i);
         plt.subplot(2,1,1)
         plt.contourf(lon,lat,snapshots[t0])
         plt.title(t0)
-    
+
         plt.subplot(2,1,2)
         plt.contourf(lon,lat,shifted[t0])
         plt.title(t0+", shifted")
@@ -107,7 +108,7 @@ def show_shiftdiff(lat,lon, snapshots, shifted):
     # plot the shifted fields to see how well they match:
     times = sorted([key for key in snapshots])
     for i,(t0,t1) in enumerate(zip(times[:-1],times[1:])):
-        plt.figure(i); 
+        plt.figure(i);
         plt.contourf(lon,lat,shifted[t0]-shifted[t1])
         plt.title(t0+"-"+t1+", shifted")
         plt.colorbar()
@@ -120,4 +121,3 @@ shifted = shift_snapshots(snapshots,u,v)
 #show_shiftdiff(lat,lon, snapshots, shifted)
 
 show_shifted(lat,lon, snapshots, shifted)
-    
